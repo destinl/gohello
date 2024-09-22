0e5d60251dc27271f3848f05b25af81b307f68a0
destinl <1374214766@qq.com>
2024-09-22 22:52:03 +0800

    fix: hello go!

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// fmt.Println("Hello, world!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	http.Handle("/", r)
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
