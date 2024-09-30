bafb44133c4b6ce0a16f951e8288dcaf2920a610
destinl <1374214766@qq.com>
2024-09-30 22:57:54 +0800

    feat: net/http包下一个i/o timeout问题

--- main.go
+++ main.go
@@ -1,13 +1,67 @@
 package main
 
-// func main() {
-// 	// fmt.Println("Hello, world!")
-// 	r := mux.NewRouter()
-// 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprintf(w, "Welcome to my website!")
-// 	})
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
+)
 
-// 	http.Handle("/", r)
-// 	fmt.Println("Server started on port 8080")
-// 	http.ListenAndServe(":8080", nil)
-// }
+var tr *http.Transport
+
+func init() {
+	tr = &http.Transport{
+		MaxIdleConns: 100,
+		Dial: func(netw, addr string) (net.Conn, error) {
+			conn, err := net.DialTimeout(netw, addr, time.Second*2) //设置建立连接超时
+			if err != nil {
+				return nil, err
+			}
+			err = conn.SetDeadline(time.Now().Add(time.Second * 3)) //设置发送接受数据超时
+			if err != nil {
+				return nil, err
+			}
+			return conn, nil
+		},
+	}
+}
+
+func main() {
+	for {
+		_, err := Get("http://www.baidu.com/")
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+}
+
+func Get(url string) ([]byte, error) {
+	m := make(map[string]interface{})
+	data, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+	body := bytes.NewReader(data)
+	req, _ := http.NewRequest("Get", url, body)
+	req.Header.Add("content-type", "application/json")
+
+	client := &http.Client{
+		Transport: tr,
+	}
+	res, err := client.Do(req)
+	if res != nil {
+		defer res.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return resBody, nil
+}
