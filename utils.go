2501aef2be25c21069249b833e0c613a483252e5
destinl <1374214766@qq.com>
2024-10-01 21:17:29 +0800

    feat: 判断字符串中字符是否全都不同

--- utils.go
+++ utils.go
@@ -5,6 +5,36 @@ import (
 	"strings"
 )
 
+func isUniqString3(s string) bool {
+	if len(s) == 0 || len(s) > 3000 {
+		return false
+	}
+	// ASCII字符字符一共有256个
+	// 256 个字符 256 = 64 + 64 + 64 + 64
+	var mark1, mark2, mark3, mark4 uint64
+	var mark *uint64
+	for _, r := range s {
+		n := uint64(r)
+		if n < 64 {
+			mark = &mark1
+		} else if n < 128 {
+			mark = &mark2
+			n -= 64
+		} else if n < 192 {
+			mark = &mark3
+			n -= 128
+		} else {
+			mark = &mark4
+			n -= 192
+		}
+		if (*mark)&(1<<n) != 0 {
+			return false
+		}
+		*mark = (*mark) | uint64(1<<n)
+	}
+	return true
+}
+
 func isRegroup(s1, s2 string) bool {
 	sl1 := len([]rune(s1))
 	sl2 := len([]rune(s2))
@@ -23,5 +53,6 @@ func isRegroup(s1, s2 string) bool {
 }
 
 func main() {
-	fmt.Println(isRegroup("你好", "世界"))
+	// fmt.Println(isRegroup("你好", "世界"))
+	fmt.Println(isUniqString3("abc"))
 }
