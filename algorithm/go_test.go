807172c9612cdc1d52d527dfa4e4097fb782de8e
destinl <1374214766@qq.com>
2024-10-06 18:49:32 +0800

    feat: 3311. 构造符合图结构的二维矩阵

--- algorithm/go_test.go
+++ algorithm/go_test.go
@@ -17,3 +17,48 @@ func TestRemainingMethods(t *testing.T) {
 	invocations := [][]int{{1, 2}, {0, 1}, {3, 2}}
 	fmt.Println(remainingMethods(n, k, invocations))
 }
+
+func TestConstructGridLayout(t *testing.T) {
+	tests := []struct {
+		n      int
+		edges  [][]int
+		result [][]int
+	}{
+		{
+			n:      4,
+			edges:  [][]int{{0, 1}, {0, 2}, {1, 3}, {2, 3}},
+			result: [][]int{{0, 1}, {2, 3}},
+		},
+		{
+			n:      5,
+			edges:  [][]int{{0, 1}, {1, 3}, {2, 3}, {2, 4}},
+			result: [][]int{{0, 1, 2, 3, 4}},
+		},
+		{
+			n:      9,
+			edges:  [][]int{{0, 1}, {0, 4}, {0, 5}, {1, 7}, {2, 3}, {2, 4}, {2, 5}, {3, 6}, {4, 6}, {4, 7}, {6, 8}, {7, 8}},
+			result: [][]int{{0, 1, 2}},
+		},
+		// {
+		// 	n:      2,
+		// 	edges:  [][]int{{0, 1}},
+		// 	result: [][]int{{0, 1}},
+		// },
+		// {
+		// 	n:      0,
+		// 	edges:  [][]int{},
+		// 	result: [][]int{},
+		// },
+	}
+
+	for _, test := range tests {
+		t.Run("Test case", func(t *testing.T) {
+			got := constructGridLayout(test.n, test.edges)
+			// if !reflect.DeepEqual(got, test.result) {
+			// 	t.Errorf("constructGridLayout(%d, %v) = %v; want %v", test.n, test.edges, got, test.result)
+			// }
+			// got := constructGridLayout(test.n, test.edges)
+			t.Log(got)
+		})
+	}
+}
--- algorithm/temp.go
+++ algorithm/temp.go
@@ -1,5 +1,83 @@
 package algorithm
 
+func constructGridLayout(n int, edges [][]int) [][]int {
+	g := make([][]int, n)
+	for _, v := range edges {
+		g[v[0]] = append(g[v[0]], v[1])
+		g[v[1]] = append(g[v[1]], v[0])
+	}
+
+	degree := make(map[int][]int)
+	for i, v := range g {
+		degree[len(v)] = append(degree[len(v)], i)
+	}
+
+	minKey, maxKey := -1, -1
+	for key := range degree {
+		if minKey == -1 || key < minKey {
+			minKey = key
+		}
+		if maxKey == -1 || key > maxKey {
+			maxKey = key
+		}
+	}
+
+	row := []int{}
+	if minKey == 1 {
+		row = append(row, degree[1][0])
+	} else if maxKey <= 3 {
+		// 矩阵只有两列
+		x := degree[2][0]
+		for _, y := range g[x] {
+			if len(g[y]) == 2 {
+				row = append(row, x)
+				row = append(row, y)
+				break
+			}
+		}
+	} else {
+		// 矩形至少有三列
+		x := degree[2][0]
+		row = append(row, x)
+		pre := x
+		x = g[x][0]
+		for len(g[x]) > 2 {
+			row = append(row, x)
+			for _, y := range g[x] {
+				if y != pre && len(g[y]) < 4 {
+					pre = x
+					x = y
+					break
+				}
+			}
+		}
+		row = append(row, x)
+	}
+	ans := [][]int{row}
+	vis := make([]bool, n)
+	for i := range vis {
+		vis[i] = false
+	}
+	count := n/len(row) - 1
+	for i := 0; i < count; i++ {
+		for _, x := range row {
+			vis[x] = true
+		}
+		nxt_row := []int{}
+		for _, x := range row {
+			for _, y := range g[x] {
+				if !vis[y] {
+					nxt_row = append(nxt_row, y)
+					break
+				}
+			}
+		}
+		ans = append(ans, nxt_row)
+		row = nxt_row
+	}
+	return ans
+}
+
 func remainingMethods(n int, k int, invocations [][]int) []int {
 
 	edges := make(map[int][]int)
