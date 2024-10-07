e3a257b13c8208d8c716892c87f32b5f3044b41d
destinl <1374214766@qq.com>
2024-10-07 15:13:05 +0800

    feat: Go常见语法问题

--- algorithm/go_test.go
+++ algorithm/go_test.go
@@ -1,9 +1,113 @@
 package algorithm
 
 import (
+	"container/heap"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"runtime"
+	"sort"
 	"testing"
 )
+
+type Student1 struct {
+	Age int
+}
+
+func TestMap(t *testing.T) {
+	kv := map[string]Student1{"menglu": {Age: 21}}
+	// kv["menglu"].Age = 22 // 不能直接修改map的值，需要先取出再赋值
+
+	s := []Student1{{Age: 21}} //切片可以，切片存储的是对结构体实例的引用
+	s[0].Age = 22
+	fmt.Println(kv, s)
+}
+
+type Student struct {
+	Name string
+}
+
+func TestEqual(t *testing.T) {
+	fmt.Println(&Student{Name: "menglu"} == &Student{Name: "menglu"})
+	fmt.Println(Student{Name: "menglu"} == Student{Name: "menglu"})
+	fmt.Println([...]string{"1"} == [...]string{"1"})
+	// 数组只能与相同纬度长度以及类型的其他数组比较，切片之间不能直接比较
+	// fmt.Println([]string{"1"} == []string{"1"})
+}
+
+func TestSlice(t *testing.T) {
+	str1 := []string{"a", "b", "c"}
+	str2 := str1[1:]
+	str2[1] = "new"
+	fmt.Println(str1)
+	str2 = append(str2, "z", "x", "y")
+	fmt.Println(str1)
+}
+
+const (
+	a = iota
+	b = iota
+)
+const (
+	name = "menglu"
+	c    = iota
+	d    = iota
+)
+
+func TestConstant(t *testing.T) {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
+
+// 输出是4，不管有没有resp.Body.Close()，这个内存泄漏问题解决了？（同一域名）
+func TestMemoryLeak(t *testing.T) {
+	num := 12
+	for index := 0; index < num; index++ {
+		resp, _ := http.Get("https://www.baidu.com")
+		_, _ = ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+	}
+	fmt.Printf("此时goroutine个数= %d\n", runtime.NumGoroutine())
+}
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool {
+	return h.IntSlice[i] > h.IntSlice[j] // 修改为最大堆的比较逻辑
+}
+
+func (h *hp) Push(v interface{}) {
+	h.IntSlice = append(h.IntSlice, v.(int))
+}
+
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
+
+func TestHeap(t *testing.T) {
+	hp := hp{}
+	heap.Push(&hp, 3)
+	heap.Push(&hp, 2)
+	heap.Push(&hp, 4)
+	heap.Push(&hp, 1)
+	fmt.Println(hp.IntSlice)
+	fmt.Println(heap.Pop(&hp).(int))
+	fmt.Println(hp.IntSlice)
+	// h := &hp{}
+	// h.Push(3)
+	// h.Push(2)
+	// h.Push(4)
+	// h.Push(1)
+
+	// fmt.Println(h.IntSlice)
+	// fmt.Println(h.Pop())
+	// fmt.Println(h.IntSlice)
+}
 
 func TestGcdValues(t *testing.T) {
 	tests := []struct {
